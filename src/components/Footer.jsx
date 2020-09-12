6dc79650cbde4a7802a490055228d518da1f5ce5
anirban <anirban552000@gmail.com>
2020-09-12 16:33:35 +0530

    Add header and footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+function Footer() {
+    return (
+            <div className="footer">
+                <p>Made with <span role="img" aria-label="heart-emoji">❤️</span> by firebolt667</p>
+            </div>
+    );
+}
+ export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <p>Currency Converter</p>
+        </div>
+    )
+}
+
+export default Navbar;
