f9ecc15991a9f3a69f872a878331711b9f7bd7e7
anirban <anirban552000@gmail.com>
2020-09-12 18:08:44 +0530

    Add css to input and select elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+// import ReactDOM from 'react-dom';
 import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
 import InputBar from './components/InputBar.jsx';
 import './App.css';
 
 function App() {
+    // const API_URL = "https://api.exchangeratesapi.io/latest?base=INR";
     return(
         <div className="app">
             <Navbar/>
@@ -14,7 +17,7 @@ function App() {
             </div>
             <Footer/>
         </div>
-    )
+    );
 }
 
 export default App;
