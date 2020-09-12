559a7ba0968bbec0f26c00fe7c961c16c2bb98d9
anirban <anirban552000@gmail.com>
2020-09-12 17:34:58 +0530

    Add InputBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
+import InputBar from './components/InputBar.jsx';
 import './App.css';
 
 function App() {
     return(
         <div className="app">
             <Navbar/>
-             
+            <div className="flex-container">
+                <InputBar className="flex-item"/>
+                <InputBar className="flex-item"/>
+            </div>
             <Footer/>
         </div>
     )
---
+++ src/components/InputBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import '../App.css';
+
+function InputBar() {
+    return (
+        <div>
+            <input type="number"/>
+            <select>
+                <option value="Hi">Hi</option>
+            </select>
+        </div>
+    );
+}
+
+export default InputBar;
